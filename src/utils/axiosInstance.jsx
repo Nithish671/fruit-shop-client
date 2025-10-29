1c67b6aabefaa96d6669f545fab4b65aeefb2536
Nithish <nnithish671@example.com>
2025-10-29 19:12:11 +0530

    modified files

--- src/utils/axiosInstance.jsx
+++ src/utils/axiosInstance.jsx
@@ -2,5 +2,5 @@ import axios from 'axios';
 
 export default axios.create({
     //baseURL: "http://localhost:5000/api",
-    baseUrl: "https://fruit-shop-server.onrender.com/api",
+    baseURL: "https://fruit-shop-server.onrender.com/api",
 });
